1e1b768cc20c0044806d24bc3faf7e2cdb0d8708
whyowisp <95976862+whyowisp@users.noreply.github.com>
2022-09-20 15:52:55 +0300

    setting up environment: logger, eslint, prettier, postman

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+
 const app = express()
-const port = 3001
+const port = 4000
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
   res.send('<div>Say Hello to my little friend</div>')
@@ -9,3 +19,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at ${port}/`)
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
