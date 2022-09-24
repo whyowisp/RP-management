d51ba2a6a44a822611ca9063aeb7885e12628cf2
whyowisp <95976862+whyowisp@users.noreply.github.com>
2022-09-24 21:44:05 +0300

    character schema ja queries ready to try in front

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+
+const { initializeCharacterData } = require('./utils/dataInitializers')
 const Character = require('./models/character')
+const character = require('./models/character')
 
 //PORT in production (fly/heroku chooses) || PORT in local environment (falls to this unless defined in process.env)
 const PORT = process.env.PORT || 4000
@@ -37,28 +40,26 @@ app.get('/api/characters', async (req, res) => {
   res.json(characters)
 })
 
-app.post('/api/characters', async (req, res) => {
-  const body = req.body
-
-  if (Object.entries(req.body).length === 0) {
-    res.status(400).end()
-    return
-  }
-
-  const character = new Character({ ...body })
-
-  const savedCharacter = await character.save()
-  res.status(201).json(savedCharacter)
+//Initializes and prefills new Character document and returns it
+app.post('/api/characters/new', async (req, res) => {
+  const character = initializeCharacterData()
+  console.log(character)
+  const newCharacter = await character.save()
+  res.status(201).json(newCharacter)
 })
 
 app.put('/api/characters/:id', async (req, res) => {
   const id = req.params.id
-  const propertyEdit = req.body
-  console.log(req.body)
-  const updatedCharacter = await Character.findByIdAndUpdate(id, propertyEdit, {
+  const updatedCharacter = await Character.findByIdAndUpdate(id, req.body, {
     new: true,
   })
   res.status(200).json(updatedCharacter)
+})
+
+app.delete('api/characters/:id', async (req, res) => {
+  const id = request.params.id
+  await Character.findByIdAndDelete(id)
+  res.status(204).end()
 })
 
 // *** SERVER REQUESTS END ***
